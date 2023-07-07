c601d026586c65e980260b9f8c6d445011a50f43
eric <erictranchell@yahoo.com>
2023-07-07 00:13:13 -0400

    html layout

--- script.js
+++
@@ -1,4 +0,0 @@
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
