63603c6153f4f1ef6881d6ca9d80629a7a2d6ab0
eric <erictranchell@yahoo.com>
2023-07-07 08:21:04 -0400

    forecast html and function

--- script.js
+++ script.js
@@ -65,6 +65,73 @@ function displayCurrentWeather(data) {
   currentWeatherContainer.appendChild(weatherIconElement);
 }
 
+// Function to display forecast data
+function displayForecast(forecasts) {
+  const forecastContainer = document.getElementById('forecast');
+  forecastContainer.innerHTML = '';
+
+  // Group forecasts by day
+  const forecastByDay = groupForecastsByDay(forecasts);
+
+  // Iterate over the forecast data for each day
+  for (const day in forecastByDay) {
+    if (forecastByDay.hasOwnProperty(day)) {
+      const forecastsForDay = forecastByDay[day];
+
+      // Take the first forecast entry of the day as representative
+      const forecast = forecastsForDay[0];
+      const forecastDate = new Date(forecast.dt * 1000);
+      const temperature = forecast.main.temp;
+      const humidity = forecast.main.humidity;
+      const windSpeed = forecast.wind.speed;
+      const weatherIcon = forecast.weather[0].icon;
+
+      // Create HTML elements for the forecast entry
+      const forecastElement = document.createElement('div');
+      forecastElement.classList.add('forecast-item');
+
+      const dateElement = document.createElement('p');
+      dateElement.textContent = forecastDate.toDateString();
+
+      const temperatureElement = document.createElement('p');
+      temperatureElement.textContent = `Temperature: ${temperature}°F`;
+
+      const humidityElement = document.createElement('p');
+      humidityElement.textContent = `Humidity: ${humidity}%`;
+
+      const windSpeedElement = document.createElement('p');
+      windSpeedElement.textContent = `Wind Speed: ${windSpeed} mph`;
+
+      const weatherIconElement = document.createElement('img');
+      weatherIconElement.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
+      weatherIconElement.alt = 'Weather Icon';
+
+      // Append elements to the container
+      forecastElement.appendChild(dateElement);
+      forecastElement.appendChild(temperatureElement);
+      forecastElement.appendChild(humidityElement);
+      forecastElement.appendChild(windSpeedElement);
+      forecastElement.appendChild(weatherIconElement);
+
+      forecastContainer.appendChild(forecastElement);
+    }
+  }
+}
+
+// Helper function to group forecasts by day
+function groupForecastsByDay(forecasts) {
+  const forecastByDay = {};
+  forecasts.forEach((forecast) => {
+    const forecastDate = new Date(forecast.dt * 1000);
+    const day = forecastDate.toDateString();
+    if (!forecastByDay[day]) {
+      forecastByDay[day] = [];
+    }
+    forecastByDay[day].push(forecast);
+  });
+  return forecastByDay;
+}
+
 const searchForm = document.getElementById('searchForm');
 
 searchForm.addEventListener('submit', function (event) {
