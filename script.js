005800c0b85aeceda30bee85e8ea28c32af6c7b7
eric <erictranchell@yahoo.com>
2023-07-07 01:27:21 -0400

    current weather fetch

---
+++ script.js
@@ -0,0 +1,20 @@
+const apiKey = '7463aec950d8030240c60a772ae32e7f';
+
+// Function to fetch weather data for a city
+function fetchWeatherData(cityName) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Process and display current weather data
+      displayCurrentWeather(data);
+console.log(data);
+      // Fetch and display forecast data
+      fetchForecastData(cityName);
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+    });
+}
+
